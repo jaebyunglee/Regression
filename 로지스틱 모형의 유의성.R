1872d58d316be696582ec97dc77653c2c3ae7b35
jaebyunglee <49424681+jaebyunglee@users.noreply.github.com>
2019-11-14 13:48:05 +0900

    Add files via upload

---
+++ 로지스틱 모형의 유의성.R
@@ -0,0 +1,33 @@
+rm(list=ls())
+set.seed(2019)
+n = 200; p = 50
+b.vec = 1/(1:p)
+x.mat = matrix(rnorm(n*p),n,p)
+y.vec = x.mat%*%b.vec+rnorm(n)
+y.vec = (y.vec>0)+0
+
+xy.df = data.frame(y.vec,x.mat)
+glm1 = glm(y.vec~.,data=xy.df,family = "binomial")
+glm2 = glm(y.vec~1,data=xy.df,family = "binomial")
+
+# likelihood ratio test
+# H0 : 모든 베타는 0이다. vs H1 : H0가 아니다.
+# p-value 가 작으면 H0 기각
+# LR test에서 p-value가 작으면 귀무가설 기각, 모형이 유의미.
+
+#1
+anova(glm2,glm1,test="LRT")
+#2
+dev = glm2$deviance - glm1$deviance
+df = glm2$df.residual - glm1$df.residual
+1-pchisq(dev,df)
+#3
+dev = glm1$null.deviance - glm1$deviance
+df = glm1$df.null - glm1$df.residual
+1-pchisq(dev,df)
+#4
+library(rms)
+lrtest(glm2,glm1)
+#5
+lrm = lrm(y.vec~.,data=xy.df)
+lrm
